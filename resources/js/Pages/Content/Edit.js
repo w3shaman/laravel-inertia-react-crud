663c4e5c4f51d3382cc0caa75368b9c6fc34e49a
w3shaman <bogeyman2007@gmail.com>
2022-07-10 10:29:00 +0700

    Gradually change class to function for using React Hooks in the future. * Update the content list page for using hooks. * Update the class is edit page to function.

--- resources/js/Pages/Content/Edit.js
+++ resources/js/Pages/Content/Edit.js
@@ -3,45 +3,39 @@ import AppHead from '../../Components/AppHead';
 import AppForm from '../../Components/AppForm';
 import AppBreadcrumb from '../../Components/AppBreadcrumb';
 
-class Edit extends React.Component {
-    constructor(props) {
-        super(props);
+function Edit(props) {
+    var title = 'Add New Content';
+    var submitUrl = route('content.add');
+    var content = undefined;
+
+    if (props.content !== undefined) {
+        title = 'Edit "' + props.content.title + '"';
+        submitUrl = route('content.edit', props.content.id);
+        content = props.content;
     }
 
-    render() {
-        var title = 'Add New Content';
-        var submitUrl = route('content.add');
-        var content = undefined;
-
-        if (this.props.content !== undefined) {
-            title = 'Edit "' + this.props.content.title + '"';
-            submitUrl = route('content.edit', this.props.content.id);
-            content = this.props.content;
-        }
-
-        return (
-            <div>
-                <AppHead title={title}/>
-                <AppBreadcrumb backLinks={[
-                        {
-                            title: 'Content List',
-                            url: route('content.index'),
-                        },
-                        {
-                            title: title,
-                            url: '',
-                        }
-                    ]} />
-                <AppForm
-                    title={title}
-                    errors={this.props.errors}
-                    flash={this.props.flash}
-                    submitUrl={submitUrl}
-                    assetUrl={this.props.assetUrl}
-                    content={content} />
-            </div>
-        );
-    }
+    return (
+        <div>
+            <AppHead title={title}/>
+            <AppBreadcrumb backLinks={[
+                    {
+                        title: 'Content List',
+                        url: route('content.index'),
+                    },
+                    {
+                        title: title,
+                        url: '',
+                    }
+                ]} />
+            <AppForm
+                title={title}
+                errors={props.errors}
+                flash={props.flash}
+                submitUrl={submitUrl}
+                assetUrl={props.assetUrl}
+                content={content} />
+        </div>
+    );
 }
 
 export default Edit;
--- resources/js/Pages/Content/Index.js
+++ resources/js/Pages/Content/Index.js
@@ -1,138 +1,119 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Inertia } from '@inertiajs/inertia';
 import { Link } from '@inertiajs/inertia-react';
 import AppHead from '../../Components/AppHead';
 import { ConfirmDialog } from '../../Components/ModalDialog';
 import Pagination from '../../Components/Pagination';
 
-class Index extends React.Component {
-    constructor(props) {
-        super(props);
+function Index(props) {
+    const [keyword, setKeyword] = useState(props.keyword === null ? '' : props.keyword);
+    const [data, setData] = useState(props.contents.data);
+    const [data_count, setDataCount] = useState(props.contents.total);
+    const [data_per_page, setDataPerPage] = useState(props.contents.per_page);
+    const [pager, setPager] = useState(props.contents.links);
+    const [show_delete_confirmation, setShowDeleteConfirmation] = useState(false);
+    const [del_content_id, setDelContentId] = useState(0);
+    const [del_content_title, setDelContentTitle] = useState('');
 
-        this.submitSearch = this.submitSearch.bind(this);
-        this.clearSearch = this.clearSearch.bind(this);
-        this.changeKeyword = this.changeKeyword.bind(this);
-        this.showDeleteConfirmation = this.showDeleteConfirmation.bind(this);
-        this.confirmDelete = this.confirmDelete.bind(this);
-        this.cancelDelete = this.cancelDelete.bind(this);
+    useEffect((e) => {
+        setData(props.contents.data);
+        setDataCount(props.contents.total);
+        setDataPerPage(props.contents.per_page);
+        setPager(props.contents.links);
+    }, [props.keyword]);
 
-        this.state = {
-            keyword: this.props.keyword === null ? '' : this.props.keyword,
-            data: this.props.contents.data,
-            data_count: this.props.contents.total,
-            data_per_page: this.props.contents.per_page,
-            pager: this.props.contents.links,
-            show_delete_confirmation: false
-        }
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        if (this.props.keyword !== prevProps.keyword) {
-            this.setState({
-                data: this.props.contents.data,
-                data_count: this.props.contents.total,
-                data_per_page: this.props.contents.per_page,
-                pager: this.props.contents.links
-            });
-        }
-    }
-
-    submitSearch(e) {
+    const submitSearch = function(e) {
         e.preventDefault();
-        Inertia.post("/", {search: 1, keyword: this.state.keyword});
+        Inertia.post("/", {search: 1, keyword: keyword});
     }
 
-    clearSearch(e) {
-        this.setState({keyword: ''});
+    const clearSearch = function(e) {
+        setKeyword('');
         Inertia.post("/", {search: 1, keyword: ''});
     }
 
-    changeKeyword(e) {
-        this.setState({keyword: e.target.value});
+    const changeKeyword = function(e) {
+        setKeyword(e.target.value);
     }
 
-    showDeleteConfirmation(id, title, e) {
+    const showDeleteConfirmation = function(id, title, e) {
         e.preventDefault();
 
-        this.setState({
-            show_delete_confirmation: true,
-            del_content_id: id,
-            del_content_title: title
-        });
+        setShowDeleteConfirmation(true);
+        setDelContentId(id);
+        setDelContentTitle(title);
     }
 
-    confirmDelete(e) {
-        window.location.href = route('content.delete', this.state.del_content_id);
+    const confirmDelete = function(e) {
+        window.location.href = route('content.delete', del_content_id);
     }
 
-    cancelDelete(e) {
+    const cancelDelete = function(e) {
         e.preventDefault();
 
-        this.setState({show_delete_confirmation: false});
+        setShowDeleteConfirmation(false);
     }
 
-    render() {
-        return (
-            <div>
-                <AppHead title={"Content List"}/>
-                <h1 className="title is-1">Content List</h1>
-                {this.props.flash.error !== null && <div className='message is-danger'><div className="message-body">{this.props.flash.error}</div></div>}
-                {this.props.flash.warning !== null && <div className='message is-warning'><div className="message-body">{this.props.flash.warning}</div></div>}
-                {this.props.flash.success !== null && <div className='message is-success'><div className="message-body">{this.props.flash.success}</div></div>}
-                <div className="content-list-button"><Link href={route('content.add')} className="button is-link">Add new</Link></div>
-                <div className="content-list-search">
-                    <form method="post" onSubmit={this.submitSearch}>
-                        <div className="field is-grouped">
-                            <div className="control">
-                                <input className="input" type="text" id="keyword" placeholder="Type keyword here..." value={this.state.keyword} onChange={this.changeKeyword} />
-                            </div>
-                            <div className="control">
-                                <button type="submit" id="search" className="button is-primary">Search</button>
-                            </div>
-                            <div className="control">
-                                <button type="button" id="clear" className="button is-light" onClick={this.clearSearch} disabled={this.state.keyword === null || this.state.keyword === undefined || this.state.keyword === ''}>Clear search</button>
-                            </div>
+    return (
+        <div>
+            <AppHead title={"Content List"}/>
+            <h1 className="title is-1">Content List</h1>
+            {props.flash.error !== null && <div className='message is-danger'><div className="message-body">{props.flash.error}</div></div>}
+            {props.flash.warning !== null && <div className='message is-warning'><div className="message-body">{props.flash.warning}</div></div>}
+            {props.flash.success !== null && <div className='message is-success'><div className="message-body">{props.flash.success}</div></div>}
+            <div className="content-list-button"><Link href={route('content.add')} className="button is-link">Add new</Link></div>
+            <div className="content-list-search">
+                <form method="post" onSubmit={submitSearch}>
+                    <div className="field is-grouped">
+                        <div className="control">
+                            <input className="input" type="text" id="keyword" placeholder="Type keyword here..." value={keyword} onChange={changeKeyword} />
                         </div>
-                    </form>
-                </div>
-                <div className="table-container">
-                    <table className="table is-striped is-hoverable">
-                        <thead>
-                            <tr>
-                                <th>Title</th>
-                                <th>Published</th>
-                                <th>Published date</th>
-                                <th colSpan='2'>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {this.state.data.length > 0 && this.state.data.map(({id, title, publish, publish_date}) => <tr key={id}>
-                                    <td>{title}</td>
-                                    <td>{publish}</td>
-                                    <td>{publish_date}</td>
-                                    <td>
-                                        <Link href={route('content.edit', id)} className="button is-info">Edit</Link>
-                                    </td>
-                                    <td>
-                                        <Link href="#" className="button is-danger" onClick={this.showDeleteConfirmation.bind(this, id, title)}>Delete</Link>
-                                    </td>
-                                </tr>
-                            )}
-                        </tbody>
-                    </table>
-                </div>
-                <ConfirmDialog
-                    id="delete-content-dialog"
-                    title="Delete Content"
-                    message={`Are you sure want to delete "${this.state.del_content_title}"?`}
-                    show={this.state.show_delete_confirmation}
-                    onConfirm={this.confirmDelete}
-                    onCancel={this.cancelDelete}
-                />
-                {this.state.data_count > this.state.data_per_page && <Pagination links={this.state.pager} />}
+                        <div className="control">
+                            <button type="submit" id="search" className="button is-primary">Search</button>
+                        </div>
+                        <div className="control">
+                            <button type="button" id="clear" className="button is-light" onClick={clearSearch} disabled={keyword === null || keyword === undefined || keyword === ''}>Clear search</button>
+                        </div>
+                    </div>
+                </form>
             </div>
-        );
-    }
+            <div className="table-container">
+                <table className="table is-striped is-hoverable">
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Published</th>
+                            <th>Published date</th>
+                            <th colSpan='2'>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.length > 0 && data.map(({id, title, publish, publish_date}) => <tr key={id}>
+                                <td>{title}</td>
+                                <td>{publish}</td>
+                                <td>{publish_date}</td>
+                                <td>
+                                    <Link href={route('content.edit', id)} className="button is-info">Edit</Link>
+                                </td>
+                                <td>
+                                    <Link href="#" className="button is-danger" onClick={(e) => showDeleteConfirmation(id, title, e)}>Delete</Link>
+                                </td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            </div>
+            <ConfirmDialog
+                id="delete-content-dialog"
+                title="Delete Content"
+                message={`Are you sure want to delete "${del_content_title}"?`}
+                show={show_delete_confirmation}
+                onConfirm={confirmDelete}
+                onCancel={cancelDelete}
+            />
+            {data_count > data_per_page && <Pagination links={pager} />}
+        </div>
+    );
 }
 
 export default Index;
