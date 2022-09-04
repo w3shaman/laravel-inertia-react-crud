cb61f5dec7e24bf8ab736e34a21d67dceb5fdce0
w3shaman <bogeyman2007@gmail.com>
2022-09-04 08:41:28 +0700

    Gradually change class to function. Change the modal dialog components from class to function.

--- resources/js/Components/ModalDialog.js
+++ resources/js/Components/ModalDialog.js
@@ -1,103 +1,74 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-class ConfirmDialog extends React.Component {
-    constructor(props) {
-        super(props);
+function ConfirmDialog(props) {
+    const [show, setShow] = useState(false);
 
-        this.onConfirm = this.onConfirm.bind(this);
-        this.onCancel = this.onCancel.bind(this);
+    useEffect((e) => {
+        setShow(props.show);
+    }, [props.show]);
 
-        this.state = {
-            show: this.props.show
-        };
-    }
-
-    onConfirm(e) {
-        if (this.props.onConfirm !== undefined) {
-            this.props.onConfirm(e);
+    const onConfirm = function(e) {
+        if (props.onConfirm !== undefined) {
+            props.onConfirm(e);
         }
     }
 
-    onCancel(e) {
-        this.setState({show: false});
-        if (this.props.onCancel !== undefined) {
-            this.props.onCancel(e);
+    const onCancel = function(e) {
+        setShow(false);
+        if (props.onCancel !== undefined) {
+            props.onCancel(e);
         }
     }
 
-    componentDidUpdate(prevProps, prevState) {
-        if (this.props.show !== null && this.props.show !== prevProps.show) {
-            this.setState(function (state, props) {
-                return {show: props.show};
-            });
-        }
-    }
-
-    render() {
-        return (
-            <div className={this.state.show === true ? "modal is-active" : "modal"}>
-                <div className="modal-background"></div>
-                <div className="modal-card">
-                    <header className="modal-card-head">
-                        <p className="modal-card-title">{this.props.title}</p>
-                    </header>
-                    <section className="modal-card-body">
-                        {this.props.message}
-                    </section>
-                    <footer className="modal-card-foot">
-                        <a href="#" className="button is-danger" onClick={this.onConfirm}>Yes</a>
-                        <a href="#" className="button is-primary" onClick={this.onCancel}>No</a>
-                    </footer>
-                </div>
+    return (
+        <div className={show === true ? "modal is-active" : "modal"}>
+            <div className="modal-background"></div>
+            <div className="modal-card">
+                <header className="modal-card-head">
+                    <p className="modal-card-title">{props.title}</p>
+                </header>
+                <section className="modal-card-body">
+                    {props.message}
+                </section>
+                <footer className="modal-card-foot">
+                    <a href="#" className="button is-danger" onClick={onConfirm}>Yes</a>
+                    <a href="#" className="button is-primary" onClick={onCancel}>No</a>
+                </footer>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
-class ErrorDialog extends React.Component {
-    constructor(props) {
-        super(props);
+function ErrorDialog(props) {
+    const [show, setShow] = useState(false);
 
-        this.close = this.close.bind(this);
+    useEffect((e) => {
+        setShow(props.show);
+    }, [props.show]);
 
-        this.state = {
-            show: this.props.show
-        };
-    }
-
-    close(e) {
-        this.setState({show: false});
-        if (this.props.onClose !== undefined) {
-            this.props.onClose(e);
+    const close = function(e) {
+        setShow(false);
+        if (props.onClose !== undefined) {
+            props.onClose(e);
         }
     }
 
-    componentDidUpdate(prevProps, prevState) {
-        if (this.props.show !== null && this.props.show !== prevProps.show) {
-            this.setState(function (state, props) {
-                return {show: props.show};
-            });
-        }
-    }
-
-    render() {
-        return (
-            <div className={this.state.show === true ? "modal is-active" : "modal"}>
-                <div className="modal-background"></div>
-                <div className="modal-card">
-                    <header className="modal-card-head">
-                        <p className="modal-card-title">{this.props.title}</p>
-                    </header>
-                    <section className="modal-card-body">
-                        {this.props.message}
-                    </section>
-                    <footer className="modal-card-foot">
-                        <a href="#" className="button is-danger" onClick={this.close}>OK</a>
-                    </footer>
-                </div>
+    return (
+        <div className={show === true ? "modal is-active" : "modal"}>
+            <div className="modal-background"></div>
+            <div className="modal-card">
+                <header className="modal-card-head">
+                    <p className="modal-card-title">{props.title}</p>
+                </header>
+                <section className="modal-card-body">
+                    {props.message}
+                </section>
+                <footer className="modal-card-foot">
+                    <a href="#" className="button is-danger" onClick={close}>OK</a>
+                </footer>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export {ConfirmDialog, ErrorDialog};
