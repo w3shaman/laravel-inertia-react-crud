5976cf63c7cfbe7939ba7a193aae4c3e68f7d1cb
w3shaman <bogeyman2007@gmail.com>
2022-09-04 08:47:53 +0700

    Gradually change class to function. Change the pager component from class to function.

--- resources/js/Components/Pagination.js
+++ resources/js/Components/Pagination.js
@@ -1,29 +1,23 @@
 import React from 'react'
 import { Link } from '@inertiajs/inertia-react';
 
-class Pagination extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div>
-                <nav className="pagination" role="navigation" aria-label="pagination">
-                    <ul className="pagination-list">
-                        {this.props.links.length > 0 && this.props.links.map(({label, url, active}, index) =>
-                            <li key={index}>
-                                {active === true
-                                    ? <Link className="pagination-link is-current" href="#" dangerouslySetInnerHTML={{ __html: label }} />
-                                    : <Link className="pagination-link" href={url} dangerouslySetInnerHTML={{ __html: label }} />
-                                }
-                            </li>
-                        )}
-                    </ul>
-                </nav>
-            </div>
-        );
-    }
+function Pagination(props) {
+    return (
+        <div>
+            <nav className="pagination" role="navigation" aria-label="pagination">
+                <ul className="pagination-list">
+                    {props.links.length > 0 && props.links.map(({label, url, active}, index) =>
+                        <li key={index}>
+                            {active === true
+                                ? <Link className="pagination-link is-current" href="#" dangerouslySetInnerHTML={{ __html: label }} />
+                                : <Link className="pagination-link" href={url} dangerouslySetInnerHTML={{ __html: label }} />
+                            }
+                        </li>
+                    )}
+                </ul>
+            </nav>
+        </div>
+    );
 }
 
 export default Pagination;
